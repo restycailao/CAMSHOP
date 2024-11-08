34864f2b416d3edb080585783f05c76a3e8a76a1
John Doe <johndoe@email.com>
2024-11-09 03:42:38 +0800

    first commit

---
+++ frontend/src/redux/api/apiSlice.js
@@ -0,0 +1,11 @@
+import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
+import { BASE_URL } from "../constants";
+
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+
+export const apiSlice = createApi({
+  baseQuery,
+  //dadagdagan pag gusto mo mag fetch ng data 
+  tagTypes: ["Product", "Order", "User", "Category"],
+  endpoints: () => ({}),
+});
